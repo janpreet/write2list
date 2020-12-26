bbd4d4c4d4827ae309173d9b8f623e40a651b76d
Janpreet Singh <janpreetsinghgill@gmail.com>
2020-12-26 11:45:46 -0500

    Added changes to the main script and release description.

---
+++ test.py
@@ -0,0 +1,7 @@
+import write2list
+
+exampleTuple=(["Hi I am the first example list"], ["123456"], ["Wow these are one too many lists for a simple example. Let's end this object here."])
+
+resultList=write2list.create(exampleTuple)
+
+print(resultList)
--- write2list/__init__.py
+++ write2list/__init__.py
@@ -1,12 +1,10 @@
-def create(*dataTuple):
+def create(dataTuple):
     try:
         response=[]
-        response.insert(0, dataTuple[0])
-        response.extend(dataTuple[1])
-        response.insert(len(response),"</td><td style='vertical-align:top'>")
-        response.extend(dataTuple[2])        
-        response.insert(len(response),"</td></tr></table>")
-        response.insert(len(response), "<small><i>NOTE: Language composition is a list of most used languages in my repositories. It is not a direct indication of my skill level.</i></small>")
+        for item in dataTuple:
+            i=dataTuple.index(item)
+            if i >= 0:
+                response.extend(dataTuple[i])
         return response
     except Exception:
         return "Error: Cannot read the tuple. Please make sure you have entered the correct values."
